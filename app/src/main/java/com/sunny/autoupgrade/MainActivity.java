f3ea757d2c0dcad672c1c0ec20bf51fa5b7937b9
yggyy <yangguang@neusoft.edu.cn>
2019-09-25 20:51:42 +0800

    install with interaction

--- app/src/main/java/com/sunny/autoupgrade/MainActivity.java
+++ app/src/main/java/com/sunny/autoupgrade/MainActivity.java
@@ -5,38 +5,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.allenliu.versionchecklib.v2.AllenVersionChecker;
-import com.allenliu.versionchecklib.v2.builder.DownloadBuilder;
-import com.allenliu.versionchecklib.v2.builder.UIData;
-import com.allenliu.versionchecklib.v2.callback.CustomInstallListener;
-import com.allenliu.versionchecklib.v2.callback.ForceUpdateListener;
-import com.allenliu.versionchecklib.v2.callback.RequestVersionListener;
-import com.google.android.play.core.appupdate.AppUpdateInfo;
-import com.google.android.play.core.appupdate.AppUpdateManager;
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
-import com.google.android.play.core.install.model.AppUpdateType;
-import com.google.android.play.core.install.model.UpdateAvailability;
-import com.google.android.play.core.tasks.Task;
 import com.sunny.autoupgrade.network.APIService;
 import com.sunny.autoupgrade.network.entities.AppConfigurationEntity;
 import com.sunny.autoupgrade.utils.Configuration;
 import com.sunny.autoupgrade.utils.StorageUtil;
-import com.sunny.autoupgrade.utils.VersionUtil;
 
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
 import java.util.List;
 
 import butterknife.BindView;
@@ -48,28 +29,22 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.ResponseBody;
 import pub.devrel.easypermissions.EasyPermissions;
-import pub.devrel.easypermissions.PermissionRequest;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-import top.wuhaojie.installerlibrary.AutoInstaller;
 
 public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
-    AutoInstaller installer;
-    private final static String TAG = "AutoUpgrade";
 
     @BindView(R.id.textView_version)
     TextView versionView;
 
     private int versionCode;
-
     private int remoteVersionCode;
 
     private static final String[] PERMISSIONS =
             {Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.REQUEST_DELETE_PACKAGES};
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,56 +52,8 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
 
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-//                installer = AutoInstaller.getDefault(this);
-
-
-        versionView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-//                upgrade();
-
-
-//                new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//
-//                        String fileName = StorageUtil.getFilePath();
-//                        installer.install(fileName);
-//                    }
-//                }).start();
-
-//                updateApp();
-//                VersionUtil.uninstall(MainActivity.this);
-
-                uninstall();
-//                finish();
-            }
-
-        });
-
 
         requestPermission();
-//
-//
-//        installer.setOnStateChangedListener(new AutoInstaller.OnStateChangedListener() {
-//            @Override
-//            public void onStart() {
-//
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//
-//            }
-//
-//            @Override
-//            public void onNeed2OpenService() {
-//
-//            }
-//        });
-
-//        updateApp("http://10.0.2.2:8080/api/v1/appConfig");
     }
 
     private void updateApp() {
@@ -138,65 +65,17 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         Uri uri = Uri.fromFile(new File(StorageUtil.getFilePath()));
         intent.setDataAndType(uri, "application/vnd.android.package-archive");
         startActivity(intent);
+        startActivityForResult(intent, 1);
     }
 
-    private void uninstall() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        VersionUtil.uninstall(this);
-    }
+        //取消，不安装apk
+        if(resultCode == 0){
 
-    private void update1() {
-
-        try {
-            VersionUtil.install(this, getPackageName(), StorageUtil.getFilePath());
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-    }
-
-    private void updateApp(String requestUrl) {
-
-        AllenVersionChecker
-                .getInstance()
-                .requestVersion()
-                .setRequestUrl(requestUrl)
-                .request(new RequestVersionListener() {
-
-                    @Nullable
-                    @Override
-                    public UIData onRequestVersionSuccess(DownloadBuilder downloadBuilder, String result) {
-
-                        downloadBuilder.setDownloadUrl("http://10.0.2.2:8080/static/app.apk")
-                                .setDirectDownload(true)
-                                .setShowNotification(false)
-                                .setShowDownloadingDialog(false)
-                                .setShowDownloadFailDialog(false)
-                                .setForceUpdateListener(new ForceUpdateListener() {
-                                    @Override
-                                    public void onShouldForceUpdate() {
-
-                                        finish();
-                                    }
-                                }).setCustomDownloadInstallListener(new CustomInstallListener() {
-                            @Override
-                            public void install(Context context, Uri apk) {
-
-                                System.out.println();
-                            }
-                        });
-
-
-//                        return UIData.create().setDownloadUrl("http://10.0.2.2:8080/static/app.apk");
-
-                        return null;
-                    }
-
-                    @Override
-                    public void onRequestVersionFailure(String message) {
-
-                    }
-                })
-                .executeMission(this);
     }
 
     private void requestPermission() {
@@ -206,14 +85,11 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
             EasyPermissions.requestPermissions(this, "请求读写磁盘的权限", 100, PERMISSIONS);
         } else {
 
-//            afterHasPermission();
+            afterHasPermission();
         }
     }
 
     private void afterHasPermission() {
-        versionCode = VersionUtil.getVersionCode();
-        logcat(String.valueOf(versionCode));
-        versionView.setText(String.valueOf(versionCode));
 
         requestAppConfig();
     }
@@ -232,6 +108,8 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         if (appConfigurationEntity.getCode() == 0) {
 
             remoteVersionCode = appConfigurationEntity.getResult().getVerCode();
+            versionCode = BuildConfig.VERSION_CODE;
+
             if (remoteVersionCode > versionCode) {
 
                 downloadAPI(appConfigurationEntity.getResult().getUrl());
@@ -263,22 +141,13 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
                     @Override
                     public void onNext(ResponseBody responseBody) {
 
-                        if (saveAPK(responseBody.byteStream())) {
+                        try {
+                            if (StorageUtil.saveAPK(responseBody.byteStream())) {
 
-
-//                            String packageName = getPackageName();
-//                            String fileName = StorageUtil.getFilePath();
-//                            installer.install(fileName);
-//
-//                            try {
-//                                VersionUtil.install(MainActivity.this, packageName, fileName);
-//                            } catch (IOException e) {
-//
-//                                e.printStackTrace();
-//                            }
-
-
-//                            upgrade();
+                                updateApp();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
                     }
 
@@ -294,77 +163,6 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
                 });
 
     }
-
-
-    private boolean saveAPK(InputStream inputStream) {
-
-        try {
-
-            return StorageUtil.saveAPK(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return false;
-    }
-
-    private void inAppUpdate() {
-
-        // Creates instance of the manager.
-        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(MainActivity.this);
-
-        // Returns an intent object that you use to check for an update.
-        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
-
-        // Checks that the platform will allow the specified type of update.
-        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
-
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                    // For a flexible update, use AppUpdateType.FLEXIBLE
-                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
-                // Request the update.
-            }
-        });
-    }
-
-    private void upgrade() {
-
-        try {
-
-//            java.lang.Process process = Runtime.getRuntime().exec("su");
-            java.lang.Process process = Runtime.getRuntime().exec("su");
-
-            DataOutputStream dataOutputStream = new DataOutputStream(process.getOutputStream());
-            // 执行pm install命令
-//            String command = "pm install -r " + apkPath + "\n";
-
-            String apkPath = StorageUtil.getFilePath();
-
-            apkPath = Configuration.testFile;
-
-            String command = "pm install -r " + apkPath + "\n";
-
-            dataOutputStream.write(command.getBytes(Charset.forName("utf-8")));
-            dataOutputStream.flush();
-            dataOutputStream.writeBytes("exit\n");
-            dataOutputStream.flush();
-            process.waitFor();
-//            Runtime.getRuntime().exec("pm install -r " + apkPath.toString() + "\n");
-            Runtime.getRuntime().exec(command);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private void logcat(String message) {
-
-        Log.e(TAG, message);
-    }
-
 
     private void requestAppConfig() {
 
@@ -396,8 +194,6 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
                     @Override
                     public void onError(Throwable e) {
 
-                        logcat(e.getLocalizedMessage());
-//                        Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
@@ -411,7 +207,7 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
     @Override
     public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
 
-        //       afterHasPermission();
+        afterHasPermission();
     }
 
     @Override
--- app/src/main/java/com/sunny/autoupgrade/utils/VersionUtil.java
+++ app/src/main/java/com/sunny/autoupgrade/utils/VersionUtil.java
@@ -14,7 +14,6 @@ import android.os.Build;
 import androidx.core.content.FileProvider;
 
 import com.sunny.autoupgrade.BuildConfig;
-import com.sunny.autoupgrade.UpgradeReceiver;
 
 import java.io.File;
 import java.io.FileInputStream;
